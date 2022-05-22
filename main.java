ab948458cf2e7746cb0112b6de78afe1855acaa0
juan-gitclub <71986216+juan-gitclub@users.noreply.github.com>
2022-05-22 12:44:52 -0400

    Add files via upload

---
+++ main.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class main {
+
+	public static void main(String[] args) {
+
+		int nthTerm;
+
+		nthTerm = fibonacciSec(10);
+
+		System.out.println("The nth term of the Fibonacci sequence is " + nthTerm);
+
+	}
+
+	public static int fibonacciSec(int num) {
+
+		if (num > 0) {
+
+			return num + fibonacciSec(num - 1);
+
+		} else {
+
+			return 0;
+
+		}
+	}
+}
